96660b0a4fb349b8b3442c2d55e7eee09681093a
Kirill Nagorny <art.ngki@gmail.com>
2020-09-17 12:06:13 +0300

    added click event

--- js/index.js
+++ js/index.js
@@ -6,3 +6,41 @@ let entries = [],
   op = '';
 
 screen.innerHTML = 0;
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener(
+    'click',
+    function () {
+      if (buttons[i].className === 'clear') {
+        screen.innerHTML = 0;
+        reset();
+      } else {
+        if (buttons[i].className != 'operator' && buttons[i].className != 'equals') {
+          entries.push(buttons[i].innerHTML);
+          screen.innerHTML = doScr(entries);
+        }
+
+        if (buttons[i].className === 'operator') {
+          op = getOperator(buttons[i].innerHTML);
+          entries.push(buttons[i].innerHTML);
+        }
+
+        if (buttons[i].className === 'equals') {
+          if (result != '') {
+            result = doOp(entries);
+          } else {
+            result = doOperation(entries);
+          }
+
+          if (isFinite(result)) {
+            screen.innerHTML = result.toFixed(2).replace(/\.?0*$/g, '');
+          } else {
+            screen.innerHTML = 'N/A';
+            reset();
+          }
+        }
+      }
+    },
+    false,
+  );
+}
